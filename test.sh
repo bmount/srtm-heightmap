fba5fe5a8903d11a47d6e53893062c3e88adeeee
Brian Mount <bvmount@gmail.com>
2013-01-21 23:44:23 -0800

    check on some extreme terrains

---
+++ test.sh
@@ -0,0 +1,7 @@
+./hgt2png --url http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_04/N37W123.hgt.zip --bbox -122.519531 37.713159 -122.384949 37.809784
+
+./hgt2png --square --url http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_04/N37W123.hgt.zip --bbox -122.519531 37.713159 -122.384949 37.809784
+
+./hgt2png --url http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_04/N36W117.hgt.zip --bbox -117.452087 36.642529 -117.053146 36.848307
+
+
